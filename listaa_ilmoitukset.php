d364595bdef3892af814fdc962f8f2c131ac815a
Timo Mämmi <timo.mammi@gmail.com>
2020-01-03 18:52:25 +0200

    Update listaa_ilmoitukset.php

--- listaa_ilmoitukset.php
+++ listaa_ilmoitukset.php
@@ -7,7 +7,7 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "
         <tr>
-            <td>" . $_POST["pvm"]. "</td>
+            <td>" . $row["pvm"]. "</td>
             <td>" . $row["aika"]. "</td>
             <td>" . $row["osastoID"]. "</td>
             <td>" . $row["tyontekijaID"]. "</td>
