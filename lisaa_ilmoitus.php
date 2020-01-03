fb219904c1470a196437b6838aab0a23d68eba93
Timo Mämmi <timo.mammi@gmail.com>
2020-01-03 19:07:28 +0200

    korjattu VALUES osio

--- lisaa_ilmoitus.php
+++ lisaa_ilmoitus.php
@@ -2,7 +2,7 @@
 
 require_once("mysqlConnection.php");
 $sql = "INSERT INTO ilmoitukset (pvm, aika, osastoID, tyontekijaID, laatu, selite) 
-        VALUES ('" . pvm . "', '" . aika . "', '" . mysqli_real_escape_string($conn, $_POST['osastoID']) . "', '" . mysqli_real_escape_string($conn, $_POST['tyontekijaID']) . "', '" . laatu . "', '" . mysqli_real_escape_string($conn, $_POST['selite']) . "')";
+        VALUES ('" . mysqli_real_escape_string($conn, $_POST['pvm']) . "', '" . mysqli_real_escape_string($conn, $_POST['aika']) . "', '" . mysqli_real_escape_string($conn, $_POST['osastoID']) . "', '" . mysqli_real_escape_string($conn, $_POST['tyontekijaID']) . "', '" . mysqli_real_escape_string($conn, $_POST['laatu']) . "', '" . mysqli_real_escape_string($conn, $_POST['selite']) . "')";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
