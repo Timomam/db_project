0ec578ad03eb7a66b8c9308511e5d858c3c5e8c2
Timomam <55140402+Timomam@users.noreply.github.com>
2020-01-03 14:54:01 +0200

    Create lisaa_ilmoitus.php

---
+++ lisaa_ilmoitus.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once("mysqlConnection.php");
+$sql = "INSERT INTO ilmoitukset (pvm, aika, osastoID, tyontekijaID, laatu, selite) 
+        VALUES ('" . pvm . "', '" . aika . "', '" . osastoID . "', '" . tyontekijaID . "', '" . laatu . "', '" . mysqli_real_escape_string($conn, $_POST['selite']) . "')";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    die("Error: " . $sql . "<br>" . $conn->error);
+}
+$conn->close();
+
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+?>
