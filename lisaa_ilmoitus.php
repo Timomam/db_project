530337487d7d66d9d2f607b1b630e25c7843d656
Timo Mämmi <timo.mammi@gmail.com>
2020-01-03 19:03:25 +0200

    Update lisaa_ilmoitus.php

--- lisaa_ilmoitus.php
+++ lisaa_ilmoitus.php
@@ -2,7 +2,7 @@
 
 require_once("mysqlConnection.php");
 $sql = "INSERT INTO ilmoitukset (pvm, aika, osastoID, tyontekijaID, laatu, selite) 
-        VALUES ('" . pvm . "', '" . aika . "', '" . osastoID . "', '" . mysqli_real_escape_string($conn, $_POST['tyontekijaID']) . "', '" . laatu . "', '" . mysqli_real_escape_string($conn, $_POST['selite']) . "')";
+        VALUES ('" . pvm . "', '" . aika . "', '" . mysqli_real_escape_string($conn, $_POST['osastoID']) . "', '" . mysqli_real_escape_string($conn, $_POST['tyontekijaID']) . "', '" . laatu . "', '" . mysqli_real_escape_string($conn, $_POST['selite']) . "')";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
